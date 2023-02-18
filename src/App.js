715425fb6ed78ad22bae9f3963d2aeed82b7cc53
farzad sanjarani <farzadsanjarani3@gmail.com>
2023-02-18 10:22:43 +1000

    co2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import Home from "./scenes/home/Home";
+
+const ScrollToTop = () => {
+  const { pathName } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathName]);
+};
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
