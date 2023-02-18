5c6e59a77cc0eb69065be354bb6821d52323bb42
farzad sanjarani <farzadsanjarani3@gmail.com>
2023-02-18 21:21:22 +1000

    co3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails.jsx";
+import CheckOut from "./scenes/checkout/CheckOut.jsx";
+import Confirmation from "./scenes/checkout/Confirmation.jsx";
+import Navbar from "./scenes/global/Navbar";
 
 const ScrollToTop = () => {
   const { pathName } = useLocation();
@@ -15,9 +18,13 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+      <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<CheckOut />} />
+          <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/state/State.js
@@ -0,0 +1,58 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isCardOpen: false,
+  cart: [],
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    setItems: (state, action) => {
+      state.items = action.payload;
+    },
+
+    addToCart: (state, action) => {
+      state.cart = [...state.cart, action.payload.item];
+    },
+
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+    },
+
+    increaseCount: (state, action) => {
+      state.cart = state.cart.map((item) => {
+        if (item.id === action.payload.id) {
+          item.count++;
+        }
+        return item;
+      });
+    },
+
+    decreaseCount: (state, action) => {
+      state.cart = state.cart.map((item) => {
+        if (item.id === action.payload.id && item.count > 1) {
+          item.count--;
+        }
+        return item;
+      });
+    },
+
+    setIsCardOpen: (state) => {
+      state.isCardOpen = !state.isCardOpen;
+    },
+  },
+});
+
+export const {
+  setItems,
+  addToCart,
+  removeFromCart,
+  increaseCount,
+  decreaseCount,
+  setIsCardOpen,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
